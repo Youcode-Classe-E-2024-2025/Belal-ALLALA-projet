8ccdf755661898a1998317efb699f9d6ece04548
belal-allala <belalallala810@gmail.com>
2024-12-26 10:12:23 +0100

    creation de la class database

---
+++ models/model.php
@@ -0,0 +1,23 @@
+<?php
+    <?php
+
+    class database {
+        private $pdo;
+    
+        public function __construct($host = 'localhost', $dbname = 'TaskFlow', $username = 'root', $password = '') {
+            $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+            $this->pdo = new PDO($dsn, $username, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_PERSISTENT => true
+            ]);
+        }
+    
+        public function query($sql, $params = []) {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt;
+        }
+    
+    }
+    
