9bc3382e86caf05523e72de4d1f6095509063c97
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-12-26 23:39:57 +0100

    separer la template en plusieurs components

---
+++ views/components/addTaskForm.php
@@ -0,0 +1,58 @@
+<div class="add-task-form overlay hidden w-screen h-screen fixed top-0 left-0 z-20 bg-black/40 flex items-center justify-center">
+   <div class="w-full rounded-xl max-w-sm p-4 bg-white relative">
+      <button class="close-btn active:scale-90 absolute top-0 right-0 p-1 text-lg leading-none">
+         <i class="fa-solid fa-xmark"></i>
+      </button>
+      <form class="space-y-2" action="/?edit=1&id=test" method="POST">
+         <div class="hidden">
+            <label for="id" class="block text-sm font-medium text-gray-900">id</label>
+            <input type="text" placeholder="id" name="id" id="id" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+         </div>
+         <div class="!mt-0">
+            <label for="title" class="block text-sm font-medium text-gray-900">title</label>
+            <input type="text" placeholder="title" name="title" id="title" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+         </div>
+         <div>
+            <label for="description" class="block text-sm font-medium text-gray-900">description</label>
+            <input placeholder="description" type="text" name="description" id="description" autocomplete="description" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+         </div>
+         <div>
+            <label for="deadline" class="block text-sm font-medium text-gray-900">deadline</label>
+            <input placeholder="YYYY-MM-DD HH:MM:SS" type="datetime" name="deadline" id="deadline" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+         </div>
+         <div>
+            <label for="statut" class="block text-sm font-medium text-gray-900">status</label>
+            <select name="statut" id="statut" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+               <option value="Todo">Todo</option>
+               <option value="Doing">Doing</option>
+               <option value="Done">Done</option>
+            </select>
+         </div>
+         <div>
+            <label for="type" class="block text-sm font-medium text-gray-900">type</label>
+            <select name="type" id="type" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+               <option value="Basic">Basic</option>
+               <option value="Bug">Bug</option>
+               <option value="Feature">Feature</option>
+            </select>
+         </div>
+         <div>
+            <label for="assignedTo" class="block text-sm font-medium text-gray-900">assign to:</label>
+            <select name="assignedTo" id="assignedTo" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+               <option value="">-- select a member --</option>
+               <option value="karim">karim</option>
+               <option value="ahmed">ahmed</option>
+               <option value="anouar">anouar</option>
+            </select>
+         </div>
+         <ul class="assigned-members">
+            <li>
+               <h6>assigned to:</h6>
+            </li>
+         </ul>
+         <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add task</button>
+         </div>
+      </form>
+   </div>
+</div>
---
+++ views/components/addTeamForm.php
@@ -0,0 +1,40 @@
+<div class="add-team-form overlay hidden w-screen h-screen fixed top-0 left-0 z-20 bg-black/40 flex items-center justify-center">
+   <div class="w-full rounded-xl max-w-sm p-6 bg-white relative">
+      <button class="close-btn active:scale-90 absolute top-0 right-0 p-1 text-lg leading-none">
+         <i class="fa-solid fa-xmark"></i>
+      </button>
+      <form class="space-y-3" action="/?edit=1&id=test" method="POST">
+         <div>
+            <label for="name" class="block text-sm/6 font-medium text-gray-900">team name</label>
+            <input type="text" placeholder="team name" name="name" id="name" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+         </div>
+
+         <div>
+            <label for="members" class="block text-sm/6 font-medium text-gray-900">members</label>
+            <div class="flex items-center gap-2">
+               <select name="type" id="members" class="selected-member block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                  <option value="">-- select a member --</option>
+                  <option value="karim">karim</option>
+                  <option value="ahmed">ahmed</option>
+                  <option value="ali">ali</option>
+                  <option value="ismail">ismail</option>
+               </select>
+               <button type="button" class="add-member-btn p-1 bg-white/50 rounded-full border border-gray-200 shadow-md text-indigo-500 hover:bg-indigo-500 hover:text-indigo-100">
+                  <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                  </svg>
+               </button>
+            </div>
+         </div>
+         <div class="team-members">
+            <h6>team members:</h6>
+            <ul class="members">
+
+            </ul>
+         </div>
+         <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add team</button>
+         </div>
+      </form>
+   </div>
+</div>
