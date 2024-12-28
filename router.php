55dac0a7abf7adf77059bd94f0084d584a152cb2
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-12-28 12:55:08 +0100

    implementer la fonctionnalite de logout et protecter la page home par redirection vers login si l'utilisateur n'est pas connecté

--- router.php
+++ router.php
@@ -1,8 +1,9 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI'])["path"];
 
-if ($path === "/") require "controllers/home.php";
+if ($path === "/" || $path === "/home") require "controllers/home.php";
 elseif ($path === "/signup") require "controllers/signup.php";
 elseif ($path === "/login") require "controllers/login.php";
+elseif ($path === "/logout") require "controllers/logout.php";
 elseif ($path === "/profile") require "controllers/profile.php";
 else require "views/404.php";
