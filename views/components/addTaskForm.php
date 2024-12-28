38136c486228ceaac498fd62b25d02e4d07de542
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2024-12-29 00:38:47 +0100

    implementer la fonctionnalité d'ajout d'une tache

--- views/components/addTaskForm.php
+++ views/components/addTaskForm.php
@@ -3,10 +3,11 @@
       <button class="close-btn active:scale-90 absolute top-0 right-0 p-1 text-lg leading-none">
          <i class="fa-solid fa-xmark"></i>
       </button>
-      <form class="space-y-2" action="/?edit=1&id=test" method="POST">
+      <form class="space-y-2" action="/home" method="post">
+         <?= isset($error) ? "<div class='text-red-600 bg-red-200 font-bold'>-- $error --</div>" : '' ?>
          <div class="hidden">
-            <label for="id" class="block text-sm font-medium text-gray-900">id</label>
-            <input type="text" placeholder="id" name="id" id="id" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+            <label for="action" class="block text-sm font-medium text-gray-900">action</label>
+            <input type="text" placeholder="action" name="action" id="action" value="add" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
          </div>
          <div class="!mt-0">
             <label for="title" class="block text-sm font-medium text-gray-900">title</label>
@@ -18,7 +19,10 @@
          </div>
          <div>
             <label for="deadline" class="block text-sm font-medium text-gray-900">deadline</label>
-            <input placeholder="YYYY-MM-DD HH:MM:SS" type="datetime" name="deadline" id="deadline" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+            <div class="flex gap-2">
+               <input type="date" name="date" id="deadline" required class="flex-[4] block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+               <input type="time" name="time" id="deadline" required class="flex-1 block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+            </div>
          </div>
          <div>
             <label for="statut" class="block text-sm font-medium text-gray-900">status</label>
@@ -36,23 +40,23 @@
                <option value="Feature">Feature</option>
             </select>
          </div>
-         <div>
-            <label for="assignedTo" class="block text-sm font-medium text-gray-900">assign to:</label>
-            <select name="assignedTo" id="assignedTo" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
-               <option value="">-- select a member --</option>
-               <option value="karim">karim</option>
-               <option value="ahmed">ahmed</option>
-               <option value="anouar">anouar</option>
-            </select>
-         </div>
-         <ul class="assigned-members">
-            <li>
-               <h6>assigned to:</h6>
-            </li>
-         </ul>
-         <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add task</button>
-         </div>
+         <?php if (!User::isPersonal()) : ?>
+            <div>
+               <label for="assignedTo" class="block text-sm font-medium text-gray-900">assign to:</label>
+               <select name="assignedTo" id="assignedTo" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm">
+                  <option value="">-- select a member --</option>
+                  <option value="karim">karim</option>
+                  <option value="ahmed">ahmed</option>
+                  <option value="anouar">anouar</option>
+               </select>
+            </div>
+            <ul class="assigned-members">
+               <li>
+                  <h6>assigned to:</h6>
+               </li>
+            </ul>
+         <?php endif; ?>
+         <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add task</button>
       </form>
    </div>
 </div>
