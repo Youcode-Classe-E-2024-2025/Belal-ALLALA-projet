722fdfe592ca6f776fed218109e963c0594b8bea
belal-allala <belalallala810@gmail.com>
2025-01-05 22:36:26 +0100

    Intégration d'un tableau Kanban pour le suivi des tâches

--- models/model.php
+++ models/model.php
@@ -886,9 +886,9 @@ class TaskUser
     public function unassign($id_task, $id_user)
     {
         $sql = "DELETE FROM task_user WHERE id_task = ? AND id_user = ?";
-        $stmt = $this->db->query($sql, [$id_task, $id_user]);
+        // $stmt = $this->db->query($sql, [$id_task, $id_user]);
 
-        // return $this->checkResult($stmt, 'Utilisateur désassigné de la tâche avec succès.');
+        return $this->checkResult($stmt, 'Utilisateur désassigné de la tâche avec succès.');
     }
 
     public function isAssigned($id_task, $id_user)
