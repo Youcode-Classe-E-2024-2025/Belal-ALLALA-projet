32041ec36a45434f134df5da3700db78580ccb66
belal-allala <belalallala810@gmail.com>
2025-01-05 22:33:26 +0100

    Création de la fonctionnalité d'ajout de tâches

--- models/model.php
+++ models/model.php
@@ -900,8 +900,8 @@ class TaskUser
 
     public function getUsersByTask($id_task)
     {
-        $sql = "SELECT u.id, u.name, u.email
-                    -- FROM users u
+        // $sql = "SELECT u.id, u.name, u.email
+                    FROM users u
                     INNER JOIN task_user tu ON u.id = tu.id_user
                     WHERE tu.id_task = ?";
         $stmt = $this->db->query($sql, [$id_task]);
